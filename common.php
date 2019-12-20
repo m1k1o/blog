86fe729403dec555efdbfb896d91a60859864f54
Miroslav Šedivý <sedivy.miro@gmail.com>
2019-12-20 18:32:04 +0100

    when debugging, show error messages

--- common.php
+++ common.php
@@ -10,6 +10,13 @@ $classLoader = new SplClassLoader(null, APP_PATH);
 $classLoader->setFileExtension('.class.php');
 $classLoader->register();
 
+// In debug mode, display errors
+if(Config::get_safe('debug', false)){
+	ini_set('display_errors', 1);
+	ini_set('display_startup_errors', 1);
+	error_reporting(E_ALL);
+}
+
 // Language
 Lang::load(empty($_GET["hl"]) ? Config::get("lang") : $_GET["hl"]);
 
