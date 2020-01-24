764b459ffd6989742ab3d10b3118615e0cd61aac
Miroslav Šedivý <sedivy.miro@gmail.com>
2020-01-24 19:14:51 +0100

    switch themes in config

--- index.php
+++ index.php
@@ -67,7 +67,7 @@ if(!empty($scripts)){
 	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
 
 	<link href="static/styles/main.css?v=<?php echo Config::get("version"); ?>" rel="stylesheet" type="text/css" />
-	<link href="static/styles/theme01.css?v=<?php echo Config::get("version"); ?>" rel="stylesheet" type="text/css" />
+	<link href="static/styles/<?php echo Config::get_safe("theme", "theme01"); ?>.css?v=<?php echo Config::get("version"); ?>" rel="stylesheet" type="text/css" />
 
 	<link href="https://fonts.googleapis.com/css?family=Open+Sans&amp;subset=all" rel="stylesheet">
 
