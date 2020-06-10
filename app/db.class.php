0ba76adc1d09c672ffa53c721cee33a7f19bc645
Miroslav Šedivý <sedivy.miro@gmail.com>
2020-06-11 01:54:39 +0200

    set pdo attributes in constructor

--- app/db.class.php
+++ app/db.class.php
@@ -46,7 +46,12 @@ class DB
 				// Username
 				Config::get('mysql_user'),
 				// Password
-				Config::get_safe('mysql_pass', '')
+				Config::get_safe('mysql_pass', ''),
+				// Set attributes
+				[
+					\PDO::ATTR_ERRMODE => \PDO::ERRMODE_EXCEPTION,
+					\PDO::ATTR_EMULATE_PREPARES => false
+				]
 			);
 
 			$this->_PDO->exec(
@@ -59,13 +64,6 @@ class DB
 		} catch (PDOException $e) {
 			throw new DBException($e->getMessage());
 		}
-
-		// When is this not set, chat does dot work, odd behavior
-		$this->_PDO->setAttribute(\PDO::ATTR_EMULATE_PREPARES, false);
-
-		// Throwing exceptions
-		$this->_PDO->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
-		//$this->_PDO->setAttribute(\PDO::MYSQL_ATTR_USE_BUFFERED_QUERY, true);
 	}
 
 	// Just flattern array to be binded : [key1, key2, [key3, [key4]]] => [key1, key2, key3, key4]
